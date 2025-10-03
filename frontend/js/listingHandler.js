4de253326671ca7e9eedb023feeb1ebf539fbd65
Naroline Lim <narolinelimswe@gmail.com>
2025-10-03 11:19:01 +1000

    created staff listing from static temporary json

---
+++ frontend/js/listingHandler.js
@@ -0,0 +1,32 @@
+
+
+async function listingHandler() {
+    const response = await fetch("data/staff.json");
+    const data = await response.json();
+
+    const staffList = document.getElementById('staff-list');
+    staffList.innerHTML = '';
+
+
+    data.forEach((staff) => {
+        const staffCard = document.createElement('div');
+        staffCard.classList.add('staff-card');
+        staffCard.innerHTML = `
+            <div class="staff-row">
+                <div class="staff-profile"></div>
+                <div class="staff-name">${staff.name}</div>
+                <div class="staff-email">${staff.email}</div>
+                <div class="staff-role">${staff.role}</div>
+            </div>
+            <div class="row-divider"></div>
+        `;
+
+        staffList.appendChild(staffCard);
+
+    });
+
+
+}
+
+
+window.addEventListener('DOMContentLoaded', listingHandler);
