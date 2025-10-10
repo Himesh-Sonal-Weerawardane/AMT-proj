6306757b45974526fd0f45ead8fc3b89bbc179f4
Naroline Lim <narolinelim@Narolines-MacBook-Air.local>
2025-10-10 17:24:35 +1100

    added moderation accordion

--- frontend/js/front-page.js
+++ frontend/js/front-page.js
@@ -1,2 +1,35 @@
 
 
+const markingAcc = document.querySelectorAll(".marking-accordion");
+const progressAcc = document.querySelectorAll(".progress-accordion");
+
+
+markingAcc.forEach(a => {
+    a.addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        const panel = this.nextElementSibling.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+})
+
+
+progressAcc.forEach(a => {
+    a.addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        const panel = this.nextElementSibling.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+})
+
+
+
