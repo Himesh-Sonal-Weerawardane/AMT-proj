2aef6a6461907dbf6dc81cbd6547f9985984f00e
Rio <ryanaga@student.unimelb.edu.au>
2025-10-19 07:13:19 +1100

    in progress - adding and deleting users

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+import express from "express"
+import multer from "multer"
+import fs from "fs"
+//https://supabase.com/docs/reference/javascript/
+export default function userRoutes(supabase) {
+    const router = express.Router()
+
+    //Add a user 
+    router.post("/add_user", async (req, res) => {
+        const {firstName, lastName, email, role } = req.body
+
+        const {data, error} = await supabase.auth.admin.createUser(
+            {
+                email,
+                password
+            }
+        )
+        const {data2, error2} = await supabase
+            .from("users")
+            .insert([{
+                firstName,
+                lastName,
+                email,
+                role,
+                userID 
+            }])
+    });
+
+    //https://dev.to/therealmrmumba/beginners-guide-to-handling-delete-requests-in-nodejs-with-express-28dh
+    router.post("/delete_user", async (req, res) => {
+        const id = parseInt(req.params.id);
+        const {data, error} = await supabase.auth.admin.deleteUser(id)
+
+        const{data2, error2} = await supabase
+            .from("users")
+            .delete()
+            .eq('userID')
+    })
+};
