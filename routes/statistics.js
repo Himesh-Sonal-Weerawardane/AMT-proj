3e2c56b6f03e898dffc4e3ad3324ae10a40b06a5
Rio <ryanaga@student.unimelb.edu.au>
2025-10-20 07:05:33 +1100

    fixed logical and syntax errors

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,37 +1,39 @@
 import express from "express"
-import multer from "multer"
-import fs from "fs"
 
 export default function statisticsRoutes(supabase) {
     const router = express.Router()
 
-    router.post("/calculate/ :assignmentID", async (req, res) => {
-        const id = parseInt(req.params.id)
+    router.post("/calculate/:assignmentID", async (req, res) => {
+        const id = parseInt(req.params.assignmentID)
 
+        // Gets marks based on given assignmentID
         const {data, error} = await supabase
             .from("marks")
             .select("mark")
-            .eq("id", id)
+            .eq("assignmentID", id)
 
         let sum = 0
         let min = 0
         let max = 0 
         let totalNum = 0
+
+        // Loops through data to find sum, min and max 
         for(let i = 0; i < data.length; i++){
             const mark = data[i].mark
             sum += score
-            total ++
+            totalNum++
             
-            if(mark > min){
-                min = score
-            }
-            if(mark < max){
+            if(mark > max){
                 max = score
+            }
+            if(mark < min){
+                min = score
             }
         }
 
         const mean = sum / totalNum
 
+        // Calculates the numerator part of the variance formula 
         let numerator = 0
         for(let i = 0; i<data.length; i++){
             numerator += (data[i].mark - mean) * (data[i].mark - mean)
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 import express from "express"
-import multer from "multer"
-import fs from "fs"
+
 //https://supabase.com/docs/reference/javascript/
 export default function userRoutes(supabase) {
     const router = express.Router()
@@ -8,13 +7,11 @@ export default function userRoutes(supabase) {
     //Add a user 
     router.post("/add_user", async (req, res) => {
         const {firstName, lastName, email, role } = req.body
-
         const {data, error} = await supabase.auth.admin.createUser(
             {
                 email,
                 password
-            }
-        )
+            })
         const {data2, error2} = await supabase
             .from("users")
             .insert([{
@@ -27,8 +24,10 @@ export default function userRoutes(supabase) {
     })
 
     //https://dev.to/therealmrmumba/beginners-guide-to-handling-delete-requests-in-nodejs-with-express-28dh
-    router.post("/delete_user/ :userID", async (req, res) => {
-        const id = parseInt(req.params.id)
+
+    //Deletes User
+    router.post("/delete_user/:userID", async (req, res) => {
+        const id = parseInt(req.params.userID)
         // const {data, error} = await supabase.auth.admin.deleteUser(id)
 
         const{data, error} = await supabase
@@ -37,7 +36,7 @@ export default function userRoutes(supabase) {
                 email: null,
                 first_name: null,
                 last_name: null,
-                is_deleted: null, 
+                is_deleted: true, 
 
             })
             .eq("userID",id)
