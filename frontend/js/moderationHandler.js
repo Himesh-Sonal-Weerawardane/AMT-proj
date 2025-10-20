15edbedb9ee8b06009167389fa505cc58f75f7c2
LIM NAROLINE <limnaroline@Narolines-MacBook-Air.local>
2025-10-21 10:27:57 +1100

    updated moderation page

--- frontend/js/moderationHandler.js
+++ frontend/js/moderationHandler.js
@@ -9,7 +9,7 @@ let rubricData;
 /* Fetching and rendering the rubric */
 document.addEventListener('DOMContentLoaded', () => {
 
-    fetch("./data/rubric.json")
+    fetch("../data/rubric.json")
         .then(response => response.json())
         .then(data => {
             rubricData = data;
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById("moderation-doc").src = rubricData.rubric.pdfFile;
             document.getElementById("moderation-title").textContent = rubricData.rubric.rubricTitle;
 
-            renderRubric(rubricData);
+            renderUnmarkedModeration(rubricData);
 
             calculateTotalScore(rubricData);
 
@@ -27,7 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-function renderRubric(data) {
+/* Render the Unmarked Moderation */
+function renderUnmarkedModeration(data) {
 
     const criteria = document.getElementById("criteria");
     criteria.innerHTML = "";
@@ -75,16 +76,33 @@ function renderRubric(data) {
 
 
         /* Criterion Description */
-        const gradeDescription = document.createElement("ul");
-        gradeDescription.classList.add("grade-description");
+        const gradeTable = document.createElement("table");
+        gradeTable.classList.add("grade-table");
 
-        element.grades.forEach(grade => {
-            const li = document.createElement("li");
-            li.innerHTML = `<strong>${grade.pointsRange}:</strong> ${grade.description.join(", ")}`;
-            gradeDescription.appendChild(li);
+        const gradeGroup = document.createElement("tr");
+        const gradeRow = document.createElement("tr");
+
+        element.grades.forEach((g) => {
+            const header = document.createElement("th");
+            header.textContent = g.grade;
+            gradeGroup.appendChild(header);
+
+            const desCell = document.createElement("td");
+
+            const desc = document.createElement("div");
+            desc.innerHTML = g.description.join("<br><br>");
+            desCell.appendChild(desc);
+
+            const gradePoints = document.createElement("div");
+            gradePoints.textContent = g.pointsRange;
+            desCell.appendChild(gradePoints);
+
+            gradeRow.appendChild(desCell);
         });
 
-        criterion.appendChild(gradeDescription);
+        gradeTable.appendChild(gradeGroup);
+        gradeTable.appendChild(gradeRow);
+        criterion.appendChild(gradeTable);
 
 
         /* Comment Section */
@@ -96,7 +114,7 @@ function renderRubric(data) {
         commentSection.classList.add("comment-section");
 
         commentPara.textContent = 'Criterion Feedback (Optional)';
-        commentPara.style.margin = "20px 20px 20px 30px";
+        commentPara.style.margin = "35px 30px 20px 30px";
         commentPara.style.fontWeight = "bold";
 
         commentSection.contentEditable = "true";
@@ -109,34 +127,211 @@ function renderRubric(data) {
 
         criteria.appendChild(criterion);
 
+        scoreBar.addEventListener("input", () => {
+            scoreView.textContent = `${scoreBar.value} / ${element.maxPoints}`;
+            highlightGrade(parseFloat(scoreBar.value), gradeTable, element.grades);
+        });
+
     });
 
 }
 
+
+/* Render the Marked Moderation */
+function renderMarkedModeration(results) {
+
+    const submitButton = document.getElementById("moderation-submit");
+    if (submitButton) submitButton.remove();
+
+    const totalScore = document.getElementById("total-score");
+    if (totalScore) {
+        totalScore.style.marginRight = "20px";
+    }
+
+    const criteria = document.getElementById("criteria");
+    criteria.innerHTML = "";
+
+    const resultStorage = results || JSON.parse(localStorage.getItem("moderationResults"));
+    if (!resultStorage) return;
+
+    resultStorage.forEach((element, index) => {
+        const criterionData = rubricData.criteria[element.criterionID]
+        const criterion = document.createElement("div");
+        criterion.classList.add("marked-criterion");
+
+        /* Criterion Title */
+        const criterionTitle = document.createElement("h4");
+        criterionTitle.classList.add("criterion-title");
+        criterionTitle.textContent = criterionData.criterion;
+        criterion.appendChild(criterionTitle);
+
+
+        const scoreWrapper = document.createElement("div");
+        scoreWrapper.classList.add("marked-score-wrapper");
+
+        /* Criterion Score Bar */
+        const scoreValue = parseFloat(element.score.split(" / ")[0]);
+        const gradeGroups = criterionData.grades;
+
+        const activeGroup = gradeGroups.find((g) => {
+            const range = g.pointsRange.match(/\(([\d.]+)\s*-\s*([\d.]+)/);
+            const min = parseFloat(range[1]);
+            const max = parseFloat(range[2]);
+            return scoreValue >= min && scoreValue <= max;
+        })
+
+        const scoreBarContainer = document.createElement("div");
+        scoreBarContainer.classList.add("score-bar-container");
+
+        const gradeColors = {
+            "High Distinction": "#6ee277",
+            "Distinction": "#d5ed59",
+            "Credit": "#fee12d",
+            "Pass": "#f49b25",
+            "Fail": "#ee4c37"
+        }
+
+        gradeGroups.forEach((g) => {
+            //const range = g.pointsRange.match(/\(([\d.]+)\s*-\s*([\d.]+)/);
+            const block = document.createElement("div");
+            block.classList.add("score-block");
+
+            if (activeGroup && g.grade === activeGroup.grade) {
+                block.classList.add("active-block");
+                block.style.backgroundColor = gradeColors[g.grade];
+                block.title = `${g.grade}: ${g.pointsRange}`;
+            } else {
+                block.title = `${g.grade}: ${g.pointsRange}`
+            }
+
+            scoreBarContainer.appendChild(block);
+        });
+
+        scoreWrapper.appendChild(scoreBarContainer);
+
+
+        const scoreInfo = document.createElement("div");
+        scoreInfo.classList.add("score-info");
+
+
+        if (activeGroup) {
+            const gradeName = document.createElement("div");
+            gradeName.classList.add("highlighted-grade");
+            gradeName.textContent = activeGroup.grade;
+            scoreInfo.appendChild(gradeName);
+        }
+
+        const scoreView = document.createElement("span");
+        scoreView.classList.add("marked-score-view");
+        scoreView.textContent = element.score;
+        scoreInfo.appendChild(scoreView);
+
+        scoreWrapper.appendChild(scoreInfo);
+        criterion.appendChild(scoreWrapper);
+
+
+        /* Criterion Description */
+        if (activeGroup) {
+            const gradeDes = document.createElement("div");
+            gradeDes.classList.add("marked-grade-description");
+
+            const des = document.createElement("div");
+            des.innerHTML = activeGroup.description.join("<br><br>");
+            gradeDes.appendChild(des);
+
+            const gradePoints = document.createElement("div");
+            gradePoints.classList.add("marked-grade-points");
+            gradePoints.textContent = activeGroup.pointsRange;
+            gradeDes.appendChild(gradePoints);
+
+            criterion.appendChild(gradeDes);
+        }
+
+        /* Criterion Comment Section */
+        const commentContainer = document.createElement("div");
+        const commentPara = document.createElement("p");
+        const commentSection = document.createElement("div");
+
+        commentContainer.classList.add("comment-container");
+        commentSection.classList.add("comment-section");
+
+        commentPara.textContent = 'Criterion Feedback';
+        commentPara.style.margin = "25px 20px 20px 25px";
+        commentPara.style.fontWeight = "bold";
+
+        commentSection.contentEditable = "false";
+        commentSection.textContent = element.comment;
+
+        commentContainer.appendChild(commentPara);
+        commentContainer.appendChild(commentSection);
+        criterion.appendChild(commentContainer);
+
+        criteria.appendChild(criterion);
+
+
+    });
+
+}
+
+/* Highlight the grade group of each criterion based on user input */
+function highlightGrade(score, gradeDes, grades) {
+    const groupDes = gradeDes.querySelectorAll("tr:nth-child(2) td");
+    const gradeHead = gradeDes.querySelectorAll("tr:first-child th");
+
+    grades.forEach((g, i) => {
+        const range = g.pointsRange.match(/\(([\d.]+)\s*-\s*([\d.]+)/);
+        const min = parseFloat(range[1]);
+        const max = parseFloat(range[2]);
+        if (score>=min && score<=max) {
+            gradeHead[i].classList.add("highlighted");
+            groupDes[i].classList.add("highlighted");
+        } else {
+            gradeHead[i].classList.remove("highlighted");
+            groupDes[i].classList.remove("highlighted");
+        }
+    });
+
+}
 
 /* Alert the user when submission is made */
 function alertSubmission() {
     const submitButton = document.getElementById("moderation-submit");
 
     submitButton.addEventListener("click", () => {
-        const scoreInput = document.querySelectorAll(".score-view");
+        const criteria = document.querySelectorAll(".criterion");
         let allScoresFilled = true;
+        const results = [];
 
-        scoreInput.forEach((element) => {
-            const empty = element.textContent.trim();
-            if (empty.startsWith("_")) {
+        criteria.forEach((criterion, index) => {
+            const scoreInput = criterion.querySelector(".score-view");
+            const commentInput = criterion.querySelector(".comment-section");
+
+            const score = scoreInput ? scoreInput.textContent.trim() : "";
+            const comment = commentInput ? commentInput.textContent.trim() : "";
+
+            if (score.startsWith("_")) {
                 allScoresFilled = false;
             }
+
+            results.push({
+                criterionID: index,
+                score: score,
+                comment: comment
+            });
+
         });
 
         if (allScoresFilled) {
             alert("Moderation submitted successfully!");
+            localStorage.setItem("moderationResults", JSON.stringify(results));
+            renderMarkedModeration(results);
         } else {
             alert("Please fill in all scores before submission!");
         }
     });
 
 }
+
 
 
 function calculateTotalScore(data) {
