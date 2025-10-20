de7a1855fa55c667abb42d4b3b2fd925dc9dc60a
Rio <ryanaga@student.unimelb.edu.au>
2025-10-21 10:30:12 +1100

    debugged and fixed errors user.js

--- routes/users.js
+++ routes/users.js
@@ -6,26 +6,42 @@ export default function userRoutes(supabase) {
 
     //Add a user 
     router.post("/add_user", async (req, res) => {
-        const {firstName, lastName, email, role } = req.body
-        const {data, error} = await supabase.auth.admin.createUser(
-            {
-                email,
-                password
-            })
-        const {data2, error2} = await supabase
-            .from("users")
-            .insert([{
-                firstName,
-                lastName,
-                email,
-                role,
-                userID 
-            }])
+        try {
+            const {firstName, lastName, email, role } = req.body
+            const {data: authData, error: authError} = await supabase.auth.admin.createUser(
+                {
+                    email,
+                    password
+                })
+
+            if (authError) return res.status(400).json({ error: authError.message })  // User has wrong email/password
+
+            const userID = authData.id
+
+            const {data, error} = await supabase
+                .from("users")
+                .insert([{
+                    firstName,
+                    lastName,
+                    email,
+                    role,
+                    userID 
+                }])
+            if (error) return res.status(400).json({ error: error.message })  // User has wrong email/password
+
+            res.json({ success: true, userData: data[0]})
+
+        } catch (err) {
+            console.error("Network or server error:", err);
+            res.status(500).json({err})
+        }
+
     })
 
     //https://dev.to/therealmrmumba/beginners-guide-to-handling-delete-requests-in-nodejs-with-express-28dh
 
     //Deletes User
+    /*
     router.post("/delete_user/:userID", async (req, res) => {
         const id = parseInt(req.params.userID)
         // const {data, error} = await supabase.auth.admin.deleteUser(id)
@@ -41,6 +57,6 @@ export default function userRoutes(supabase) {
             })
             .eq("userID",id)
 
-    })
+    })*/
     return router
 }
